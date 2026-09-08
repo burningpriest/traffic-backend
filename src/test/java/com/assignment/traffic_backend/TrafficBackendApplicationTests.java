d0134d72c9b1ac62091d867ddd534751e43fa187
himanshu <himanshu6295@gmail.com>
2026-09-07 21:52:17 -0700

    first commit

---
+++ src/test/java/com/assignment/traffic_backend/TrafficBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.traffic_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrafficBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
